4bbad0748fc9a124f370952f21dba6a3980114ea
Ekundayo <ekundayomustapha92@gmail.com>
2025-09-16 17:38:34 +0100

    fixing error name

--- src/pages/auth/forgotpassword/ForgotPassword.jsx
+++ src/pages/auth/forgotpassword/ForgotPassword.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import styles from "./forgotpassword.module.css";
+import styles from "../forgotpassword/forgotPassword.module.css";
 
 const ForgotPassword = () => {
   const [formData, setFormData] = useState({ email: "" });
